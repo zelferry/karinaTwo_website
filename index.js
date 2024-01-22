3648ae3a4517ffcc61df9ab24cee3329ae4852c9
zelferry <karinabot04@gmail.com>
2024-01-21 22:52:59 -0300

    fix 8

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "views")));
 //app.use(express.static("./views"));
 
-router.use(require("express-session")({
+app.use(require("express-session")({
     secret: process.env.SECRET,
     ...require("./json/session.json")
 }));
